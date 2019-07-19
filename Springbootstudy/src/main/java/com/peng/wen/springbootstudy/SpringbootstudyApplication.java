d44fce43a22c792fd2edace53db2a4213922dcd0
GreatBoyLi <leewenpeng@126.com>
2019-07-19 18:09:03 +0800

    增加Freemarker模板

--- Springbootstudy/src/main/java/com/peng/wen/springbootstudy/SpringbootstudyApplication.java
+++ Springbootstudy/src/main/java/com/peng/wen/springbootstudy/SpringbootstudyApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootstudyApplication {
 
+	/**
+	 * 应用启动
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootstudyApplication.class, args);
 	}
---
+++ Springbootstudy/src/main/java/com/peng/wen/springbootstudy/controller/FreemarkerController.java
@@ -0,0 +1,42 @@
+package com.peng.wen.springbootstudy.controller;
+
+import com.peng.wen.springbootstudy.pojo.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * freemarker 模板引擎测试
+ */
+@Controller
+@RequestMapping("/ftl")
+public class FreemarkerController {
+
+    /**
+     * 资源文件对象
+     */
+    @Autowired
+    private Resource resource;
+
+    /**
+     * 测试index页面
+     * @param map ModuleMap
+     * @return String .ftl文件的位置
+     */
+    @RequestMapping("/index")
+    public String index(ModelMap map){
+        map.addAttribute("resource",resource);
+        return "freemarker/index";
+    }
+
+    /**
+     * 测试center页面
+     * @return center页面的位置
+     */
+    @RequestMapping("/center")
+    public String Center(){
+        return "freemarker/center/center";
+    }
+
+}
