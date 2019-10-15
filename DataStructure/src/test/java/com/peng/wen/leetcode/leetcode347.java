d9c99d9b2b79df9073cadf392bdb5edf1ff3c971
GreaBoyLi <leewenpeng@126.com>
2019-10-15 23:00:04 +0800

    leet-code347问题，前K个高频单词

--- DataStructure/src/test/java/com/peng/wen/leetcode/leetcode347.java
+++ DataStructure/src/test/java/com/peng/wen/leetcode/leetcode347.java
@@ -1,10 +1,23 @@
 package com.peng.wen.leetcode;
 
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class leetcode347 {
+
+    private class Freq implements Comparable<Freq> {
+        public int e, freq;
+
+        public Freq(int e, int freq){
+            this.e = e;
+            this.freq = freq;
+        }
+
+        @Override
+        public int compareTo(Freq o) {
+            return this.freq - o.freq;
+        }
+    }
+
     public List<Integer> topKFrequent(int[] nums, int k) {
         Map<Integer,Integer> map = new TreeMap<Integer, Integer>();
         for (int i : nums){
@@ -14,6 +27,20 @@ public class leetcode347 {
                 map.put(i,1);
             }
         }
-        return null;
+
+        PriorityQueue<Freq> pq = new PriorityQueue<Freq>();
+        for (int key : map.keySet()){
+            if (pq.size() < k){
+                pq.add(new Freq(key,map.get(key)));
+            }else if(map.get(key) > pq.peek().freq){
+                pq.remove();
+                pq.add(new Freq(key, map.get(key)));
+            }
+        }
+        LinkedList<Integer> res = new LinkedList<Integer>();
+        while(!pq.isEmpty()){
+            res.add(pq.remove().e);
+        }
+        return res;
     }
 }
